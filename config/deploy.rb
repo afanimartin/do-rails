87f5bcebbaba90c47fb7e635b6595b80db5b58ff
afanimartin <afani.dev@gmail.com>
2023-06-08 12:42:54 +0200

    Update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.3"
 
 set :application, "do-rails"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:afanimartin/do-rails.git"
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
